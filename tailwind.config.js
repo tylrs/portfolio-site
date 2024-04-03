134054befbc36dab07f363cf7dc2ecaf7d50b209
Taylor Galloway <galloway.taylor@gmail.com>
2024-04-02 19:48:23 -0500

    Add default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          cream: "FDF0D5",
+          navy: "003049",
+          red: "C1121F",
+          maroon: "780000",
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
