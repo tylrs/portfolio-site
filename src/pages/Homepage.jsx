4f6cf18a4f1555ff215e8b554ed6a429b49e1430
Taylor Galloway <galloway.taylor@gmail.com>
2024-04-03 20:30:14 -0500

    Add hover states to home page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -11,28 +11,29 @@ const Homepage = () => {
             <p className="text-primary-cream max-w-[32ch]">Fullstack Software Engineer with two years of experience working as a consultant for start-ups and medium-sized companies. Experienced in the front-end and back-end working with React, Ruby on Rails, JavaScript, TypeScript, and React Native. Worked on greenfield web apps, migrated APIs, and developed a mobile app. </p>
             <div className="flex gap-4">
                 <a href="https://github.com/tylrs">
-                    <GitHubIcon className="h-14" />
+                    <GitHubIcon className="h-14 fill-primary-cream hover:fill-primary-red" />
                 </a>
                 <a href="https://www.linkedin.com/in/taylor-galloway/">
-                    <LinkedInIcon className="h-14" />
+                    <LinkedInIcon className="h-14 fill-primary-cream hover:fill-primary-red" />
                 </a>
                 <a href="https://medium.com/@galloway.taylor/list/taylor-galloway-blog-posts-7765d04e82d6">
-                    <MediumIcon className="h-14" />
+                    <MediumIcon className="h-14 group" />
+                    {/* hover state styling is on svg */}
                 </a>
             </div>
         </div>
-        <div className="grid grid-rows-5 grid-cols-3 col-start-6 col-span-7 text-primary-cream border-primary-cream text-2xl">
-            <button className="row-start-2 border-r-2">About Me</button>
+        <div className="grid grid-rows-5 grid-cols-3 col-start-6 col-span-7 text-primary-cream text-2xl">
+            <button className="row-start-2 border-r-2 border-primary-cream hover:text-primary-red">About Me</button>
             <div className="row-start-2 col-start-2 border-2 [border-image:linear-gradient(to_top,#FDF0D5_60%,transparent_50%)_0_100%_10_0]"></div>
-            <button className="row-start-2 col-start-3">Projects</button>
-            <div className="row-start-3 border-b-2 border-t-2 border-r-2"></div>
+            <button className="row-start-2 col-start-3 hover:text-primary-red">Projects</button>
+            <div className="row-start-3 border-b-2 border-t-2 border-r-2 border-primary-cream"></div>
             <div className="row-start-3 col-start-2 flex justify-center items-center p-6 border-t-2 border-b-2 border-r-2 border-primary-cream">
-                <button className="bg-[#4F4789] text-primary-cream p-8 rounded-xl">Resumé</button>
+                <button className="bg-[#4F4789] text-primary-cream p-8 rounded-xl hover:text-primary-red">Resumé</button>
             </div>
-            <div className="row-start-3 col-start-3 border-b-2 border-t-2"></div>
+            <div className="row-start-3 col-start-3 border-b-2 border-t-2 border-primary-cream"></div>
             <div className="row-start-4 col-start-1 border-r-2 [border-image:linear-gradient(to_bottom,#FDF0D5_30%,transparent_30%)_0_100%_10_0]"></div>
-            <div className="row-start-4 col-start-2 border-r-2"></div>
-            <button className="row-start-4 col-start-3">Contact Me</button>
+            <div className="row-start-4 col-start-2 border-r-2 border-primary-cream"></div>
+            <button className="row-start-4 col-start-3 hover:text-primary-red">Contact Me</button>
         </div>
     </div>
 }
