a51c8c140f60dac761aa73f2c0594d1382794731
Taylor Galloway <galloway.taylor@gmail.com>
2024-04-03 20:06:51 -0500

    Add links to icons

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import GitHubIcon from "../assets/icons/githubIcon.svg?react"
 import LinkedInIcon from "../assets/icons/linkedinIcon.svg?react"
+import MediumIcon from "../assets/icons/mediumIcon.svg?react"
 
 const Homepage = () => {
     return <div className="grid grid-cols-12 bg-primary-navy h-screen py-20 px-32">
@@ -9,8 +10,15 @@ const Homepage = () => {
             </div>
             <p className="text-primary-cream max-w-[32ch]">Fullstack Software Engineer with two years of experience working as a consultant for start-ups and medium-sized companies. Experienced in the front-end and back-end working with React, Ruby on Rails, JavaScript, TypeScript, and React Native. Worked on greenfield web apps, migrated APIs, and developed a mobile app. </p>
             <div className="flex gap-4">
-                <GitHubIcon />
-                <LinkedInIcon />
+                <a href="https://github.com/tylrs">
+                    <GitHubIcon className="h-14" />
+                </a>
+                <a href="https://www.linkedin.com/in/taylor-galloway/">
+                    <LinkedInIcon className="h-14" />
+                </a>
+                <a href="https://medium.com/@galloway.taylor/list/taylor-galloway-blog-posts-7765d04e82d6">
+                    <MediumIcon className="h-14" />
+                </a>
             </div>
         </div>
         <div className="grid grid-rows-5 grid-cols-3 col-start-6 col-span-7 text-primary-cream border-primary-cream text-2xl">
